c31aa7fb4ccccf88548633f75c09ebe0d0a95154
DenisKondratov <denys.kondratov@gmail.com>
2021-01-18 00:03:16 +0200

    Add js for links

--- main.js
+++ main.js
@@ -3,14 +3,45 @@
     let containerDisable = document.querySelector('.container');
     let containerMenu = document.querySelector('.containerMenu');
     let divInvisible = document.querySelectorAll('.menuButtonItem');
+    let logoLink = document.querySelectorAll('.logo__link');
+    let activCounter = document.querySelectorAll('.container__stockContainer-link');
+
     menuButton.addEventListener('click', () => {
         containerDisable.classList.toggle('containerDisable');
         containerMenu.classList.toggle('menuButtonActive');
         menuButton.classList.toggle('activeFigure');
-        for (let i = 0; i < divInvisible.length; i++){
+        for (let i = 0; i < divInvisible.length; i++) {
             divInvisible[i].classList.toggle('menuButtonItemInvisible')
         }
-        console.log(divInvisible)
     })
+
+
+    logoLink[0].onclick = function () {
+        logoLink[0].classList.add('logo__linkActive');
+        logoLink[1].classList.remove('logo__linkActive');
+        logoLink[2].classList.remove('logo__linkActive');
+
+    }
+    logoLink[1].onclick = function () {
+        logoLink[1].classList.add('logo__linkActive');
+        logoLink[0].classList.remove('logo__linkActive');
+        logoLink[2].classList.remove('logo__linkActive');
+
+    }
+    logoLink[2].onclick = function () {        
+        logoLink[2].classList.add('logo__linkActive');
+        logoLink[0].classList.remove('logo__linkActive');
+        logoLink[1].classList.remove('logo__linkActive');
+
+    }
+
+    for(i = 0; i < activCounter.length; i++) {
+        activCounter[i].onclick = function(e){
+            e.preventDefault(); 
+            activCounter[i].classList.remove('activCounter');
+            this.classList.add('activCounter');
+        }
+    }
+
 
 }());
